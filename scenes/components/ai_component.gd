c9950a7c22ca8a5a982eba8cae25ea4332882caa
Frank Hartman <actuallyfrank@pm.me>
2023-12-26 15:29:22 +0100

    add change to closest target after x time

--- scenes/components/ai_component.gd
+++ scenes/components/ai_component.gd
@@ -32,7 +32,6 @@ func _process(_delta: float) -> void:
 func updateState() -> void:
 	if canAttack():
 		state = State.ATTACKING
-		print("ATTACKING")
 		return
 
 	if target == null:
@@ -70,14 +69,23 @@ func resetTarget() -> void:
 	if actorsInRange.size() > 0:
 		target = actorsInRange[0]
 
+func sort(a: HitboxComponent, b: HitboxComponent) -> bool:
+	if a == null or b == null:
+		return false
+
+	if global_position.distance_to(a.global_position) > global_position.distance_to(b.global_position):
+		return false
+	return true
+
 func _on_detection_zone_area_entered(area:Area2D):
 	if !HitUtils.canHit(area, teamComponent.team):
 		return
 
-	target = area
-
 	if actorsInRange.find(area) == -1:
 		actorsInRange.append(area)
+
+	if target == null:
+		target = actorsInRange[0]
 
 
 func _on_detection_zone_area_exited(area: Area2D):
@@ -91,3 +99,8 @@ func _on_detection_zone_area_exited(area: Area2D):
 
 	if isCurrentTarget(area):
 		resetTarget()
+
+
+func _on_target_timer_timeout():
+	actorsInRange = DistanceUtils.sortByDistance(global_position, actorsInRange) 
+	target = actorsInRange[0]
---
+++ scripts/utils/distance_utils.gd
@@ -0,0 +1,23 @@
+class_name DistanceUtils 
+
+static func sortByDistance(currentPosition: Vector2, actors: Array) -> Array:
+	if actors.size() == 0:
+		return []
+	
+	var sortedActors: Array = actors
+
+	var sortFunc = func(a: Node2D, b: Node2D) -> bool:
+		return sort(a, b, currentPosition)
+
+	sortedActors.sort_custom(sortFunc)
+	return sortedActors
+
+
+
+static func sort(a: Node2D, b: Node2D, currentPosition: Vector2) -> bool:
+	if a == null or b == null:
+		return false
+
+	if currentPosition.distance_to(a.global_position) > currentPosition.distance_to(b.global_position):
+		return false
+	return true
